62a5a39559c4abbae4efa88a50f767623ea26607
changach-3000 <sharon.changach@student.moringaschool.com>
2023-03-14 08:43:06 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400){
+    return "This one is on me!";
+  }else if(distance >= 2000 && distance < 2500){
+     return "I will gladly take your thirty bucks.";
+  }else if(distance > 2500){
+    return "No can do.";
+  }
+  scuberGreetingForFeet(199);
+  scuberGreetingForFeet(2000);
+  scuberGreetingForFeet(2501);
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city === 'NYC') ? `Ok, sounds good.` : `No go.`;
+  
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+ function switchOnCharmFromTip(tip) {
+   switch (tip) {
+     case 'generous':
+     return('Thank you so much.');
+     break;
+     case 'not as generous':
+     return('Thank you.');
+     break;
+     default:
+     return('Bye.');
+     break;
+   }
+ }
